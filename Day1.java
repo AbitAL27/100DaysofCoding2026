456ec72c8d1ce811d63c09d08a6e81f283fba3ed
AbitAL27 <4bid728@gmail.com>
2026-09-02 20:31:31 +0800

    Day1.java

--- Day1.java
+++ Day1.java
@@ -1,8 +1,15 @@
-jenis komentar 1:
+public class Day1{
+
+  public static void main(String[] args) {
+
+    jenis komentar 1:
 //Nama saya Abid
 
-jenis komentar 2:
+    jenis komentar 2:
 /*
 Saya abid alfaras
 berasal dari kec.tapango
  */
+  
+    
+  }
