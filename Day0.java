772672149ca9e0e928c3d4045180507d079c306a
AbitAL27 <4bid728@gmail.com>
2026-09-02 17:59:55 +0800

    Create Day0.java

---
+++ Day0.java
@@ -0,0 +1,15 @@
+public class Perkenalan {
+    public static void main(String[] args) {
+        String nama = "Abid";
+        int umur = 20;
+        String jurusan = "Teknik Informatika";
+        String kampus = "UNSULBAR";
+
+        System.out.println("=== DATA DIRI ===");
+        System.out.println("Halo, perkenalkan nama saya " + nama);
+        System.out.println("Umur saya " + umur + " tahun");
+        System.out.println("Saya mahasiswa jurusan " + jurusan);
+        System.out.println("Saya berkuliah di " + kampus);
+        System.out.println("Senang bisa berkenalan dengan kalian!");
+    }
+}
