65147644f04b8634ad485aba9c48106f100a7ba7
AbitAL27 <4bid728@gmail.com>
2026-09-03 09:38:16 +0800

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,22 @@
+/**
+ * day2
+ */
+public class day2 {
+
+    public static void main(String[] args) {
+         // Menggunakan println()
+        System.out.println("=== BIODATA DIRI ===");
+        System.out.println("Nama        : ABIT ALFARAS");
+        System.out.println("Jurusan     : Teknik Informatika");
+
+        // Menggunakan print()
+        System.out.print("Kampus      : ");
+        System.out.print("Universitas Sulawesi Barat");
+
+        System.out.println();
+
+        // Menggunakan printf()
+        System.out.printf("Umur        : %d tahun%n", 20);
+        System.out.printf("TINGGI      : %.2f%n", 158.55);
+    }
+}
