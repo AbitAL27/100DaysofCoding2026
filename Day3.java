b7ae15a2bb45a9f679f242d369fffa806207d34d
AbitAL27 <4bid728@gmail.com>
2026-09-04 13:14:22 +0700

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,28 @@
+/**
+ * day3
+ */
+public class day3 {
+
+    public static void main(String[] args) {
+        
+
+        System.out.println("Kota\nSolo");
+
+        System.out.println("Nama\tJokowi");
+
+        System.out.println("Dia berkata, \"Saya akan lawan!\"");
+
+        System.out.println("C:\\Java");
+
+        char karakter = '\'';
+        System.out.println(karakter);
+ 
+        System.out.println("PRABOWO\bO");
+
+        System.out.println("PRABOWO\rJOKOWI");
+
+        System.out.println("NOMOR 1\fNOMOR 2");
+
+        System.out.println("\u0041");
+    }
+}
