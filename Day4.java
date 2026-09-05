0535febc13b8ccc87bfb50780075756f6e93b2fa
AbitAL27 <4bid728@gmail.com>
2026-09-05 17:36:22 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,17 @@
+/**
+ * day4
+ */
+public class day4 {
+
+    public static void main(String[] args) {
+        byte angkaKecil = 100;
+        short angkaSedang = 30000;
+        int angkaBesar = 2000000000;
+        long angkaSangatBesar = 9000000000L;
+
+        System.out.println("byte  : " + angkaKecil);
+        System.out.println("short : " + angkaSedang);
+        System.out.println("int   : " + angkaBesar);
+        System.out.println("long  : " + angkaSangatBesar);
+    }
+}
