470719e292095f88a28a421a44a7180ef47cf0fa
AbitAL27 <4bid728@gmail.com>
2026-09-07 18:15:12 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,13 @@
+/**
+ * day6
+ */
+public class day6 {
+
+    public static void main(String[] args) {
+        char inisial = 'A';
+        boolean saatIniRupiahMelemah = true;
+
+        System.out.println(inisial);
+        System.out.println(saatIniRupiahMelemah);
+    }
+}
