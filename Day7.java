adaf7432a6d43e20c2058ba4315fad8a194d20f5
AbitAL27 <4bid728@gmail.com>
2026-09-08 13:17:29 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,15 @@
+/**
+ * day7
+ */
+public class day7 {
+
+    public static void main(String[] args) {
+        String nama = "jokowi";
+        String askot = "solo,surakarta";
+        String kataKata = "\"saya akan lawan\"";
+
+        System.out.println("NAMA        :"+nama);
+        System.out.println("ASAL KOTA   :"+askot);
+        System.out.println("KATA-KATA   :"+kataKata);
+    }
+}
