3813096e95355d26bbd913e390da1d75575f9028
AbitAL27 <4bid728@gmail.com>
2026-09-10 11:33:12 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,20 @@
+/**
+ * day9
+ */
+public class day9 {
+
+    public static void main(String[] args) {
+        
+
+        final int hari = 7;
+        final int bulan = 12;
+        final double nilai = 89.99;
+
+
+        System.out.println("TOTAL HARI      :" + hari );
+        System.out.println("TOTAL BULAN     :" + bulan);
+        System.out.println("NILAI AKHIR     :" + nilai);
+
+
+    }
+}
