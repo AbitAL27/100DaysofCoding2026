10e2bc57dbff336b4b431c5e3143e281155952f8
AbitAL27 <4bid728@gmail.com>
2026-09-11 19:28:42 +0800

    Create day10.java

---
+++ day10.java
@@ -0,0 +1,40 @@
+/**
+ * day10
+ */
+public class day10 {
+
+    public static void main(String[] args) {
+        
+        System.out.println("===== BATAS TIPE DATA JAVA =====");
+
+        System.out.println("\nBYTE");
+        System.out.println("Minimum : " + Byte.MIN_VALUE);
+        System.out.println("Maksimum: " + Byte.MAX_VALUE);
+
+        System.out.println("\nSHORT");
+        System.out.println("Minimum : " + Short.MIN_VALUE);
+        System.out.println("Maksimum: " + Short.MAX_VALUE);
+
+        System.out.println("\nINT");
+        System.out.println("Minimum : " + Integer.MIN_VALUE);
+        System.out.println("Maksimum: " + Integer.MAX_VALUE);
+
+        System.out.println("\nLONG");
+        System.out.println("Minimum : " + Long.MIN_VALUE);
+        System.out.println("Maksimum: " + Long.MAX_VALUE);
+
+        System.out.println("\nFLOAT");
+        System.out.println("Nilai positif terkecil : " + Float.MIN_VALUE);
+        System.out.println("Nilai terbesar         : " + Float.MAX_VALUE);
+
+        System.out.println("\nDOUBLE");
+        System.out.println("Nilai positif terkecil : " + Double.MIN_VALUE);
+        System.out.println("Nilai terbesar         : " + Double.MAX_VALUE);
+
+        System.out.println("\nCHAR");
+        System.out.println("Nilai minimum char     : " + (int) Character.MIN_VALUE);
+        System.out.println("Nilai maksimum char    : " + (int) Character.MAX_VALUE);
+
+
+    }
+}
