904b88e6c877696ab100c95e250ee495f9f3ee93
AbitAL27 <4bid728@gmail.com>
2026-09-17 15:22:23 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+/**
+ * day16
+ */
+public class day16 {
+
+    public static void main(String[] args) {
+        
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan Angka pertama\t: ");
+        int angka1 = input.nextInt();
+        System.out.print("Masukan Angka kedua\t: ");
+        int angka2 = input.nextInt();
+        int sisa = angka1 % angka2;
+
+
+        System.out.println("Sisa pembagian\t\t: " + sisa);
+
+
+
+
+
+
+        input.close();
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+}
